6c55d3455d92f7ffd470eb5304128a105951af38
SirenGlitch <110285018+SirenGlitch@users.noreply.github.com>
2024-02-18 21:11:48 +0000

    Remove mason.nvim from custom.plugins

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,11 +1,14 @@
 local plugins = {
   {
-    "williamboman/mason.nvim",
-    lazy = false
-  },
-  {
     "thinca/vim-quickrun",
     event = "BufRead"
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
   }
 }
 return plugins
