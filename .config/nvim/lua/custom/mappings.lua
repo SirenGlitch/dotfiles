a4f6404b90ec13a9b37cb1d1a53f1f9fa358a66a
SirenGlitch <110285018+SirenGlitch@users.noreply.github.com>
2024-04-25 16:45:51 +0100

    Edit NVChad mappings to use new syntax (ish)

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,10 +1,14 @@
-local M = {}
+local map = vim.keymap.set
 
-M.general ={
+local mappings = {
   n = {
     -- QuickRun on F5
     ["<F5>"] = { ":QuickRun<CR>", "Run the current buffer", opts= { noremap = true, silent = true } }
-  }
+  },
 }
 
-return M
+for mode, maps in pairs(mappings) do
+  for key, val in pairs(maps) do
+    map(mode, key, val[1], val[2])
+  end
+end
