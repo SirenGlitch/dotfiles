cecdc42a6f0ca31f11de1be674ab08f15b5a8da4
SirenGlitch <110285018+SirenGlitch@users.noreply.github.com>
2024-04-28 01:07:43 +0100

    (Finally) get custom mappings working again

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,29 +1,8 @@
-require("nvchad.mappings")
+require "nvchad.mappings"
 
--- local map = vim.keymap.set
+-- add yours here
 
--- local mappings = {
-  -- n = {
-    -- ["<F5>"] = { ":QuickRun<CR>", "Run the current buffer", opts= { noremap = true, silent = true } },
- -- },
--- }
---
--- for mode, maps in pairs(mappings) do
- -- for key, val in pairs(maps) do
-   -- map(mode, key, val[1], val[2])
- -- end
--- end
-
+local map = vim.keymap.set
 
 -- QuickRun on F5
--- map("n", "<F5>", "<cmd>QuickRun<CR>", {desc = "Run the current buffer"})
---
---
--- M.general ={
---   n = {
---    -- QuickRun on F5
---     ["<F5>"] = { ":QuickRun<CR>", "Run the current buffer", opts= { noremap = true, silent = true } }
---   }
--- }
---
--- return M
+map("n", "<F5>", "<cmd>QuickRun<CR>", {desc = "Run the current buffer"})
