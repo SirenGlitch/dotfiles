1a5f9211a69ea38edd5221151fe55886bcf2482f
SirenGlitch <110285018+SirenGlitch@users.noreply.github.com>
2024-04-28 12:02:57 +0100

    Make some changes to NvChad statusbar UI

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,6 +1,34 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme='catppuccin' }
+M.ui = {
+  theme='catppuccin',
+
+  hl_override = {
+
+    St_pos_text = {
+      fg = "lightbg",
+      bg = "green",
+    },
+    St_cwd_text = {
+      fg = "lightbg",
+      bg = "red",
+    },
+  },
+
+  statusline = {
+    modules = {
+      cwd = function()
+        local icon = "%#St_cwd_icon#" .. "󰉋 "
+        local name = vim.loop.cwd()
+        return (vim.o.columns > 85 and ("%#St_cwd_sep#" .. "" .. icon .. name .. "%#St_cwd_sep#" .. "")) or ""
+      end,
+
+      cursor = function()
+       return "%#St_pos_sep#" .. "" .. "%#St_pos_icon# %#St_pos_text# %p %% " .. "%#St_pos_sep#" .. ""
+      end
+    },
+  },
+}
 
 return M
