63dd76d151a2190230576deeab652370f7102320
SirenGlitch <frazerjamesrose@outlook.com>
2024-05-26 10:29:26 +0100

    Add dap mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,3 +6,10 @@ local map = vim.keymap.set
 
 -- QuickRun on F5
 map("n", "<F5>", "<cmd>QuickRun<CR>", {desc = "Run the current buffer"})
+
+-- DAP mappings
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>")
+map("n", "<leader>dpr", function()
+  require('dap-python').test_method()
+  end
+)
