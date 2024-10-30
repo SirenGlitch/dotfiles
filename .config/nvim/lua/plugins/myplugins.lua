0fbe9314b42eced6cb5538ba25ccee9f2899caad
SirenGlitch <frazerjamesrose@outlook.com>
2024-10-30 22:17:45 +0000

    Add lazygit.nvim

--- .config/nvim/lua/plugins/myplugins.lua
+++ .config/nvim/lua/plugins/myplugins.lua
@@ -1,5 +1,23 @@
 local plugins = {
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Run LazyGit" },
+    },
+  },
+  {
     "mrcjkb/rustaceanvim",
     ft = "rust",
     opts = function()
