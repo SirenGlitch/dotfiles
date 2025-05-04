f28ccd7b086a42913edf2e92685db5a05b1fbcca
SirenGlitch <frazerjamesrose@outlook.com>
2025-05-05 00:46:48 +0100

    Add omp to yazi config
    No idea how to make it sync with VCS, I'll work it out

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,11 +1,12 @@
 require("git"):setup()
 require("full-border"):setup()
+require("omp"):setup()
 
 Status:children_add(function(self)
-	local h = self._current.hovered
-	if h and h.link_to then
-		return " -> " .. tostring(h.link_to)
-	else
-		return ""
-	end
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
 end, 3300, Status.LEFT)
